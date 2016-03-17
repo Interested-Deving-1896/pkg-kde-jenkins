28065684f3b2b4e4c842e0642aa93b56302822e0
Maximiliano Curia <maxy@gnuservers.com.ar>
2016-03-17 13:11:41 +0100

    Only optionally delete the jobs

--- generate.sh
+++ generate.sh
@@ -1,7 +1,31 @@
 #!/bin/sh
 
+set -e
+
+delete="false"
+
+for i in "$@"; do
+    case "$i" in
+        --delete|-d)
+            delete="true"
+            shift
+            ;;
+        *)
+            break
+            ;;
+    esac
+done
+
+if ${delete}; then
+    if [[ ! -e out ]]; then
+        mkdir out
+    fi
+    find out -printf '%P\n' | \
+        xargs /usr/bin/jenkins-job-builder --conf jjb.ini delete
+fi
+
 python3 jjb-builder.py -o jobs/projects.yaml @framework.paths
 
-find out -printf '%P\n' | \
-    xargs /usr/bin/jenkins-job-builder --conf jjb.ini delete
+./test.sh
+
 jenkins-job-builder --conf jjb.ini update -r jobs

f9c691daaa0e65f4a9466ea30966f2e3b27a2b0a
Maximiliano Curia <maxy@gnuservers.com.ar>
2016-03-17 13:19:09 +0100

    Now that the pollscm seems to be working, drop the pollscm from the trigger script

--- jobs/prepare_trigger.sh
+++ jobs/prepare_trigger.sh
@@ -3,7 +3,12 @@
 set -x
 set -e
 
-cd "${WORKSPACE}"
+if [ ! -d "${WORKSPACE}/repo" ]; then
+    # not scm polled, skip
+    exit 1
+fi
+
+cd "${WORKSPACE}/repo"
 
 expand_tag () {
     # TODO: The upstream tag format should be configurable
@@ -17,43 +22,24 @@ expand_tag () {
     python -c "print '${upstream_tag_format}' % {'version': '${version}'}"
 }
 
-remote_ref="refs/remotes/debian/master"
-
-cd repo
-# Check if there is local branch
-if [ -z "$(git branch -l)" ]; then
-    git checkout --track -b master refs/remotes/debian/master
-    exit 0
-fi
-
-# The Git Client plugin fails to produce the trigger
-git fetch --all --tags
-git rev-list --left-right --count HEAD.."${remote_ref}" | \
-    read ahead behind
-
-if [ ${behind} -gt 0 ]; then
-    git rebase "${remote_ref}"
-    exit 0
-fi
-
 # Check for new upstream releases
 
-# TODO: Detect native packages
+# Detect native packages
+if grep -q 'native' debian/source/format; then
+    exit 1
+fi
+
 version=$(dpkg-parsechangelog -S version)
 epochless_version=${version##*:}
 upstream_version=${epochless_version%%-*}
 # TODO: What about dfsg tags?
 upstream_tag=$(expand_tag "${upstream_version}")
-upstream_commit=$(git rev-parse "${upstream_tag}")
-if [ -z "${upstream_commit}" ]; then
-    echo "Missing upstream tag for version ${upstream_version}" > /dev/stderr
-    exit 1
-fi
 
 # ignore the "unstable" (*.*.80 or up) releases
-release_tag=$(git tag --sort='version:refname' \
-    --contains "${upstream_commit}" -l "$(expand_tag '*')" | \
-    egrep -v '[89][0-9]+$' | fgrep -v "${upstream_tag}" | tail -1)
+release_tag=$(git tag --sort='version:refname' -l "$(expand_tag '*')" | \
+    egrep -v '([89][0-9]+|(rc|alpha|beta)[0-9]*)$' | \
+    sed -n "/${upstream_tag}/+1,\$p" | \
+    tail -1)
 
 if [ -z "${release_tag}" ]; then
     # No new release

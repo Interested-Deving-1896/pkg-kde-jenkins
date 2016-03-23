22386fc49a56cad8a4ce7a6742900bf0effb3b7f
Maximiliano Curia <maxy@gnuservers.com.ar>
2016-03-23 22:14:22 +0100

    Push the new changelog entry

--- jobs/prepare_build.sh
+++ jobs/prepare_build.sh
@@ -101,6 +101,10 @@ gbp buildpackage --git-verbose --git-upstream-tag="$(expand_tag)" \
     --git-export-dir="${export_dir}" --git-dist="${distribution}" \
     --git-overlay -S -us -uc --changes-option="-DDistribution=${distribution}"
 
+# Push
+git push
+
+# Upload source package locally
 version=$(dpkg-parsechangelog -S version)
 epochless_version=${version##*:}
 cd "${export_dir}"

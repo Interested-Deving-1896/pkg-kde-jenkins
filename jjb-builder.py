306ea1db63e4b61ca57d1c07de020ff471a9c08f
Maximiliano Curia <maxy@gnuservers.com.ar>
2016-03-29 17:40:43 +0200

    Add license information

--- jjb-builder.py
+++ jjb-builder.py
@@ -1,7 +1,23 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+# jjb-builder, jenkins job builder builder
+#  Copyright © 2016 Maximiliano Curia <maxy@gnuservers.com.ar>
+
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU General Public License for more details.
+
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, see <http://www.gnu.org/licenses/>.
+
 '''
-Generate jenkings-job-builder yaml file from a set of Debian package
+Generate jenkins-job-builder yaml file from a set of Debian package
 repositories.
 
 Optionally it will generate projects from the directories hierachy.

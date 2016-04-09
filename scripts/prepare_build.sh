3aaf330dbdfe1f5c846565c5e54e5bd0c7698b3c
Maximiliano Curia <maxy@gnuservers.com.ar>
2016-04-09 18:48:14 +0200

    Oh, dragoons here too

--- scripts/prepare_build.sh
+++ scripts/prepare_build.sh
@@ -227,6 +227,6 @@ epochless_version=${version##*:}
 cd "${export_dir}"
 # Fix permissions, else dput tries to do it remotely, which fails if the file
 # is already processed
-# find -maxdepth 1 -type f -exec chmod 0644 '{}' '+'
+find -maxdepth 1 -type f -exec chmod 0644 '{}' '+'
 # dput -u local "${source_name}_${epochless_version}_source.changes"
 dupload -t local --nomail "${source_name}_${epochless_version}_source.changes"

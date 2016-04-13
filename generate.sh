393294840de45724c78a7c1a3bc93c42a1905839
Maximiliano Curia <maxy@gnuservers.com.ar>
2016-04-13 19:11:31 +0200

    Add applications

--- generate.sh
+++ generate.sh
@@ -27,7 +27,7 @@ fi
 python3 jjb-builder.py -o jobs/projects.yaml \
     --local-vcs "ssh://freak.gnuservers.com.ar/git" \
     --basedir ~/debian \
-    @framework.paths @plasma.paths
+    @framework.paths @plasma.paths @applications.paths
 
 ./test.sh
 

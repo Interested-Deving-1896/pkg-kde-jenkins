ffda4b0a9988297c3a660d156849786314e7c06c
Maximiliano Curia <maxy@gnuservers.com.ar>
2016-04-16 21:16:43 +0200

    Also clean up the build logs

--- scripts/clean_build.sh
+++ scripts/clean_build.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-export_dir="$(pwd)/build"
+if [ -z "$WORKSPACE" ]; then
+    export WORKSPACE="$(pwd)"
+fi
+export_dir="$WORKSPACE/build"
 
 echo "Clean build directory"
 
-rm -rf "${export_dir}"
+rm -rf "$export_dir"
+rm -rf "$WORKSPACE"/arch=*

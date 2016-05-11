a48481851e78b760f03fd3e5265f69a7f116014f
Maximiliano Curia <maxy@gnuservers.com.ar>
2016-05-11 12:20:30 +0200

    Retry on hash sum mismatch

--- scripts/build_build.sh
+++ scripts/build_build.sh
@@ -115,7 +115,18 @@ case "$DISTRIBUTION" in
         ;;
 esac
 
-sbuild "${SBUILD_ARGS[@]}" "$dsc_file"
+if ! sbuild "${SBUILD_ARGS[@]}" "$dsc_file"; then
+    # Check if it was a Hash Sum mismatch and retry, once
+    buildlog=$(find -type f -name '*.build')
+    if grep -q 'E: Failed to fetch.*Hash Sum mismatch' "$buildlog"; then
+        # Avoid confusing hooks and the like
+        rm "$buildlog"
+        sbuild "${SBUILD_ARGS[@]}" "$dsc_file"
+    else
+        # Some other error, no retry
+        exit 1
+    fi
+fi
 
 echo "Call post-build hooks"
 
